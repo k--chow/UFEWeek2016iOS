a4615a27c993619267a942175189da6d09dcefed
kchow23 <kchow23@ufl.edu>
2016-01-30 13:16:15 -0500

    Initial

---
+++ UFE-Week/AboutViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AboutViewController.swift
+//  UFE-Week
+//
+//  Created by Kevin Chow on 1/30/16.
+//  Copyright © 2016 Kevin Chow. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBOutlet weak var navBar: UINavigationBar!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController!.navigationBar.barTintColor = UIColor(red: 0, green: 0.6667, blue: 0.1647, alpha: 1.0)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
